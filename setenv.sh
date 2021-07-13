db3b97de26e53d222b28fef715d92a1b3f145e77
Charandas Batra <charandas@stormforge.io>
2021-07-12 18:57:43 -0700

    First take with Argo workflows

---
+++ setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+export ARGO_SERVER=localhost:2746
+export ARGO_HTTP1=true
+export ARGO_SECURE=true
+export ARGO_BASE_HREF=
+export ARGO_TOKEN=
+export ARGO_NAMESPACE=argo-workflows
+export ARGO_INSECURE_SKIP_VERIFY=true
